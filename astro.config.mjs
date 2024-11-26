3f6e19bb4983e01fceaba15b8ca18c74d13f21dc
Maria <ev.maria.pro@gmail.com>
2024-11-26 12:22:56 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,5 +19,5 @@ export default defineConfig({
     
   },
 
-  adapter: vercel()
+  adapter: vercel(),
 });
